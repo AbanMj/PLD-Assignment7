c880ce90a0232d7621c0bdd9c02d17b1f07a36d9
Mary Joy Aban <abanmaryjoy0@gmail.com>
2021-11-30 14:38:45 +0800

    It counts the consonants

--- Assign7.1.py
+++ Assign7.1.py
@@ -5,11 +5,19 @@
 
 sentence =input("What do you want to say to me?(2 sentences): \n     ")
 vowels=0
+consonants=0
 
 for i in sentence:
     if(i == 'a'or i == 'e'or i == 'i'or i == 'o'or i == 'u' or
        i == 'A'or i == 'E'or i == 'I'or i == 'O'or i == 'U' ):
            vowels=vowels+1
-
+    elif (i == 'b' or i == 'c' or i == 'd' or i == 'f' or i == 'g' or i == 'h' or i == 'j' or i == 'k' or i == 'l' or 
+        i == 'm' or i == 'n' or i == 'p' or i == 'q' or i == 'r' or i == 's' or i == 't' or i == 'v' or i == 'w' or 
+        i == 'x' or i == 'y' or i == 'z' or
+        i == 'B' or i == 'C' or i == 'D' or i == 'F' or i == 'G' or i == 'H' or i == 'J' or i == 'K' or i == 'L' or 
+        i == 'M' or i == 'N' or i == 'P' or i == 'Q' or i == 'R' or i == 'S' or i == 'T' or i == 'V' or i == 'W' or 
+        i == 'X' or i == 'Y' or i == 'Z'):
+            consonants=consonants+1
 
 print(f"Vowels: \n     The sentences have {vowels} vowels.")
+print(f"Consonants: \n     The sentences have {consonants} consonants.")
